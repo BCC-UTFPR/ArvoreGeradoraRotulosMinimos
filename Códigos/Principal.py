b078c16666061ccc5ba0a4cd5325e67b2ce1f6fc
LUIZ FELIPE FRONCHETTI DIAS <a1552309@21e100-507807.(none)>
2015-12-01 17:27:08 -0200

    Atualização

--- Códigos/Principal.py
+++ Códigos/Principal.py
@@ -24,6 +24,8 @@ class MLST:
 			return False
 			
 	def generate_chromosome(self,graph,size):
+		print 'Iniciando processo pra gerar indivíduo (cromossomo)...'
+
 		line = 0
 		column = 0
 		adjacency_list = []
@@ -62,6 +64,7 @@ class MLST:
 		return label_list_used
 
 	def crossover(self,S,graph,graph_size):
+		print 'Iniciando processo de crossover...'
 
 		frequency = []
 		frequency.extend([0 for i in range(0,len(S))])
@@ -73,7 +76,7 @@ class MLST:
 					if label_column == each_label:						
 						frequency[position] = frequency[position] + 1
 						
-		print 'População (S): ' + str(S)
+		print 'Rótulos: ' + str(S)
 		print 'Frequência/Rótulo: ' + str(frequency)
 		print 'Quantidade de rótulos usados: ' + str(len(S))
 		print '\n'
@@ -105,14 +108,19 @@ class MLST:
 				for each_value in each_list:
 					if each_value in node_list: node_list.remove(each_value)
 		
-		print 'Foi gerado um novo subgrafo (T) com sucesso.'
+		print 'Foi gerado um novo subgrafo (t) com sucesso.'
+		print 'Lista de adjacência: '
+
 		for i,each in enumerate(adjacency_list):
 			print str(i + 1) + ' -> ' + str(each)
 		label_used.sort()
-		print 'Rótulos utilizados em (T): ' + str(label_used)
+		print 'Rótulos utilizados em (t): ' + str(label_used)
+		print 'Quantidade de rótulos (após crossover): ' + str(len(label_used))
+		print '\n'
 		return label_used
 		
 	def mutation(self,S,graph,size):
+		print 'Iniciando processo de mutação...'
 		label_not_used = []
 		label_not_used.extend([i for i in range(1,size)])
 		T = []
@@ -121,7 +129,7 @@ class MLST:
 		while flag == 0:
 			current_label = random.choice(label_not_used)
 			if not current_label in S:
-				print 'Adicionado o rótulo ' + str(current_label) + ' em S.'
+				print 'Adicionado o rótulo ' + str(current_label) + ' em T.'
 				S.append(current_label)
 				S.sort()
 				flag = 1
@@ -135,6 +143,10 @@ class MLST:
 					if label_column == each_label:						
 						frequency[position] = frequency[position] + 1
 		
+		print 'T: ' + str(S)
+		print 'Frequência (T): ' + str(frequency)
+		print '\n'
+		
 		node_list = []
 		node_list.extend([i for i in range(1,size)])
 		T = S
@@ -145,9 +157,10 @@ class MLST:
 		
 		while(self.exists_not_connected(node_list)):
 			frequency_index = frequency.index(min(frequency))
-			node_removed = S[frequency_index]
+			node_removed = T[frequency_index]
 			T.remove(node_removed)
-			
+			print 'T Atual: ' + str(T)
+			print T
 			for label in T:
 				for label_line in graph:
 					for label_column in label_line:
@@ -169,7 +182,7 @@ class MLST:
 				break
 		
 		for each in adjacency_list:
-			print each
+			pass
 
 		
 	
